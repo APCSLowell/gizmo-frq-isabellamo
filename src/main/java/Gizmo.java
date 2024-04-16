e25eef6865e3099d52c99ae895970042a527f810
isabellamo <142440827+isabellamo@users.noreply.github.com>
2024-04-15 23:15:53 -0700

    Update Gizmo.java

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -1,33 +1,50 @@
-public class Gizmo
-{
+public class Gizmo {
   private String maker;
   private boolean isElec;
   
   /** Returns the name of the manufacturer of this Gizmo. */
-  public String getMaker()
-  {
+  public String getMaker() {
     return maker;
   }
+  
   /** Returns true if this Gizmo is electronic, and false
   * otherwise.
   */
-  public boolean isElectronic()
-  {
+  public boolean isElectronic() {
     return isElec;
   }
+  
   /** Returns true if this Gizmo is equivalent to the Gizmo
   * object represented by the
   * parameter, and false otherwise.
   */
-  public boolean equals(Object other)
-  {
+  public boolean equals(Object other) {
     boolean sameMaker = maker.equals(((Gizmo)other).getMaker());
     boolean bothElec = isElec == ((Gizmo)other).isElectronic();
     return sameMaker && bothElec;
   }
-  public Gizmo(String s, boolean e)
-  {
+  
+  public Gizmo(String s, boolean e) {
     maker = s;
     isElec = e;
   }
+
+  public int countElectronicsByMaker(String maker) {
+        int count = 0;
+        for (Gizmo gizmo : purchases) {
+            if (gizmo.isElectronic() && gizmo.getMaker().equals(maker)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean hasAdjacentEqualPair() {
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            if (purchases.get(i).equals(purchases.get(i + 1))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
