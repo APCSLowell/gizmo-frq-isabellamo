89795c6487ae890e02d16c49c9c3b09848d708f9
isabellamo <142440827+isabellamo@users.noreply.github.com>
2024-04-15 23:27:20 -0700

    Update Gizmo.java

--- src/main/java/Gizmo.java
+++ src/main/java/Gizmo.java
@@ -1,58 +1,33 @@
-public class Gizmo {
+public class Gizmo
+{
   private String maker;
   private boolean isElec;
   
-  public String getMaker() {
+  /** Returns the name of the manufacturer of this Gizmo. */
+  public String getMaker()
+  {
     return maker;
   }
-  
-  public boolean isElectronic() {
+  /** Returns true if this Gizmo is electronic, and false
+  * otherwise.
+  */
+  public boolean isElectronic()
+  {
     return isElec;
   }
-  
-  public boolean equals(Object other) {
-    if (!(other instanceof Gizmo)) {
-      return false;
-    }
-    Gizmo otherGizmo = (Gizmo) other;
-    boolean sameMaker = this.maker.equals(otherGizmo.getMaker());
-    boolean bothElec = this.isElec == otherGizmo.isElectronic();
+  /** Returns true if this Gizmo is equivalent to the Gizmo
+  * object represented by the
+  * parameter, and false otherwise.
+  */
+  public boolean equals(Object other)
+  {
+    boolean sameMaker = maker.equals(((Gizmo)other).getMaker());
+    boolean bothElec = isElec == ((Gizmo)other).isElectronic();
     return sameMaker && bothElec;
   }
-  
-  public Gizmo(String s, boolean e) {
+  public Gizmo(String s, boolean e)
+  {
     maker = s;
     isElec = e;
-  }
-}
-
-import java.util.ArrayList;
-
-public class OnlinePurchaseManager {
-  private ArrayList<Gizmo> purchases;
-
-  public OnlinePurchaseManager(ArrayList<Gizmo> purchases) {
-    this.purchases = purchases;
-  }
-  
-  public int countElectronicsByMaker(String maker) {
-    int count = 0;
-    for (Gizmo gizmo : purchases) {
-        if (gizmo.isElectronic() && gizmo.getMaker().equals(maker)) {
-            count++;
-        }
-    }
-    return count;
-  }
-
-  public boolean hasAdjacentEqualPair() {
-    for (int i = 0; i < purchases.size() - 1; i++) {
-        Gizmo current = purchases.get(i);
-        Gizmo next = purchases.get(i + 1);
-        if (current.equals(next)) {
-            return true;
-        }
-    }
-    return false;
   }
 }
